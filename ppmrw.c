0e827c48a75b6775c13abb759a58c4978bde9bc4
BrunoBob <tessierb95@gmail.com>
2016-09-11 14:30:29 -0800

    Added function to get conversion type

--- ppmrw.c
+++ ppmrw.c
@@ -1,8 +1,45 @@
 #include "ppmrw.h"
 
+/*
+* RETURN
+*
+* 0 = DONE
+* 1 = Wrong number of arg
+* 2 = Wrong type of conversion
+*/
+
 int main(int argc, char *argv[]){
+  char conversionType;  // 0 = P3 to P6 | 1 = P6 to P3
+
+  char *inputName;
+  char *OutputName;
+
+  FILE *InputFile;
+  FILE *OutputFile;
+
+  int width, height;
+
+  //Verification of argument number
   if(argc < 4){
     fprintf(stderr, "Error : Convention is -> ppmrw type_of_conversion input.ppm ouput.ppm\n");
+    return 1;
   }
+
+  //Get conversion type
+  if((conversionType = getConversionType(argv[1])) == -1){
+    fprintf(stderr, "Error : Type of conversion must be  3 (convert to P3) or 6 (convert to P6)\n");
+    return 2;
+  }
+
   return 0;
+}
+
+char getConversionType(char* arg){
+  if(!strcmp(arg, "3")){
+    return 0;
+  }
+  else if(!strcmp(arg, "6")){
+    return 1;
+  }
+  return -1;
 }
--- ppmrw.h
+++ ppmrw.h
@@ -1,2 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+char getConversionType(char* arg);
