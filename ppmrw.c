aeb95cc5d1279332dabdd56afb59fe3f017c3f3e
BrunoBob <tessierb95@gmail.com>
2016-09-11 16:03:22 -0800

    Added function to open file and check format

--- ppmrw.c
+++ ppmrw.c
@@ -6,18 +6,15 @@
 * 0 = DONE
 * 1 = Wrong number of arg
 * 2 = Wrong type of conversion
+* 3 = Error opening file : file dont exist
+* 4 = Format not ppm P3 or ppm P6
 */
 
 int main(int argc, char *argv[]){
-  char conversionType;  // 0 = P3 to P6 | 1 = P6 to P3
+  char conversionType;  // 0 = convert to P6 | 1 = convert to P3
 
-  char *inputName;
-  char *OutputName;
-
-  FILE *InputFile;
-  FILE *OutputFile;
-
-  int width, height;
+  ppmImage inputImage = NULL;
+  ppmImage outputImage = NULL;
 
   //Verification of argument number
   if(argc < 4){
@@ -31,6 +28,20 @@ int main(int argc, char *argv[]){
     return 2;
   }
 
+  //Open input file
+  inputImage = openImage(inputImage,argv[2]);
+  if(inputImage == NULL){
+    fprintf(stderr, "Error : Cannot open file %s\n", argv[2]);
+    return 3;
+  }
+  if((inputImage->type != 3) && (inputImage->type != 6)){
+    fprintf(stderr, "Error : Wrong input file format, expect ppm file in format P3 or P6 \n");
+    return 4;
+  }
+
+  //Close input and output files
+  closeImage(inputImage);
+
   return 0;
 }
 
@@ -42,4 +53,35 @@ char getConversionType(char* arg){
     return 1;
   }
   return -1;
+}
+
+ppmImage openImage(ppmImage image, char* name){
+    image = (ppmImage) malloc(sizeof(*image));
+    image->name = name;
+    image->type = 0;
+
+    //Opening file
+    image->file = fopen(name, "r");
+    if(image->file == NULL){
+      free(image);
+      return NULL;
+    }
+
+    //Checking file format
+    char* buffer = (char*)malloc(2*sizeof(char));
+    fread(buffer, sizeof(char), 2, image->file);
+    if(!strcmp(buffer, "P3")){
+      image->type = 3;
+    }
+    else if(!strcmp(buffer, "P6")){
+      image->type = 6;
+    }
+    return image;
+}
+
+void closeImage(ppmImage image){
+  if(image->file != NULL){
+    fclose(image->file);
+  }
+  free(image);
 }
--- ppmrw.h
+++ ppmrw.h
@@ -2,4 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct{
+  char* name;
+  FILE* file;
+  char type;
+  int width, height;
+} *ppmImage;
+
+
 char getConversionType(char* arg);
+ppmImage openImage(ppmImage image, char* name);
+void closeImage(ppmImage image);
