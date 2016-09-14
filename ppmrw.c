7638d0cb88e4a56ea45d40e114f93ebcbed1d4f4
BrunoBob <tessierb95@gmail.com>
2016-09-14 15:31:29 -0700

    Conversion function work in every possible case

--- ppmrw.c
+++ ppmrw.c
@@ -65,73 +65,76 @@ char getConversionType(char* arg){
 }
 
 ppmImage openImage(char* name){
-    ppmImage image = (ppmImage) malloc(sizeof(*image));
-    image->name = name;
-    image->type = 0;
-    image->width = image->height = 0;
+  ppmImage image = (ppmImage) malloc(sizeof(*image));
+  image->name = name;
+  image->type = 0;
+  image->width = image->height = 0;
 
-    //Opening file
-    image->file = fopen(name, "r");
-    if(image->file == NULL){
-      free(image);
-      return NULL;
-    }
+  //Opening file
+  image->file = fopen(name, "r");
+  if(image->file == NULL){
+    free(image);
+    return NULL;
+  }
 
-    //Checking file format
-    char* buffer = (char*)malloc(2*sizeof(char));
-    fread(buffer, sizeof(char), 2, image->file);
-    if(!strcmp(buffer, "P3")){
-      image->type = 3;
-    }
-    else if(!strcmp(buffer, "P6")){
-      image->type = 6;
-    }
-    free(buffer);
-
-    //Reading comment
-    buffer = (char*)malloc(sizeof(char));
-    fread(buffer, sizeof(char), 1, image->file); //read end of line car
-    fread(buffer, sizeof(char), 1, image->file); //Read first car to see if comment
-    while((buffer != NULL) && (!strcmp(buffer, "#"))){
-      while(strcmp(buffer, "\n")){
-        fread(buffer, sizeof(char), 1, image->file);
-      }
-      fread(buffer, sizeof(char), 1, image->file);
-    }
-
-    //Get image size
-    int numberSize = 1;
-    char* number = (char*)malloc(sizeof(char)*numberSize);
-    number[numberSize-1] = buffer[0];
-    while((fread(buffer, sizeof(char), 1, image->file)) && (strcmp(buffer, " "))){
-      if(buffer != NULL){
-        numberSize++;
-        realloc(number,sizeof(char)*numberSize);
-        number[numberSize-1] = buffer[0];
-      }
-    }
-    image->width = atoi(number);
-    free(number);
-
-    numberSize = 0;
-    number = (char*)malloc(sizeof(char)*numberSize);
-    while((fread(buffer, sizeof(char), 1, image->file)) && (strcmp(buffer, "\n"))){
-      if(buffer != NULL){
-        numberSize++;
-        realloc(number,sizeof(char)*numberSize);
-        number[numberSize-1] = buffer[0];
-      }
-    }
-    image->height = atoi(number);
-    free(number);
-
-    //Reading max color value
-    do{
-      fread(buffer, sizeof(char), 1, image->file);
-    }while(strcmp(buffer, "\n"));
-
-    free(buffer);
+  //Checking file format
+  char* buffer = (char*)malloc(2*sizeof(char));
+  fread(buffer, sizeof(char), 2, image->file);
+  if(!strcmp(buffer, "P3")){
+    image->type = 3;
+  }
+  else if(!strcmp(buffer, "P6")){
+    image->type = 6;
+  }
+  free(buffer);
+  if(image->type == 0){
     return image;
+  }
+
+  //Reading comment
+  buffer = (char*)malloc(sizeof(char));
+  fread(buffer, sizeof(char), 1, image->file); //read end of line car
+  fread(buffer, sizeof(char), 1, image->file); //Read first car to see if comment
+  while((buffer != NULL) && (!strcmp(buffer, "#"))){
+    while(strcmp(buffer, "\n")){
+      fread(buffer, sizeof(char), 1, image->file);
+    }
+    fread(buffer, sizeof(char), 1, image->file);
+  }
+
+  //Get image size
+  int numberSize = 1;
+  char* number = (char*)malloc(sizeof(char)*numberSize);
+  number[numberSize-1] = buffer[0];
+  while((fread(buffer, sizeof(char), 1, image->file)) && (strcmp(buffer, " "))){
+    if(buffer != NULL){
+      numberSize++;
+      realloc(number,sizeof(char)*numberSize);
+      number[numberSize-1] = buffer[0];
+    }
+  }
+  image->width = atoi(number);
+  free(number);
+
+  numberSize = 0;
+  number = (char*)malloc(sizeof(char)*numberSize);
+  while((fread(buffer, sizeof(char), 1, image->file)) && (strcmp(buffer, "\n"))){
+    if(buffer != NULL){
+      numberSize++;
+      realloc(number,sizeof(char)*numberSize);
+      number[numberSize-1] = buffer[0];
+    }
+  }
+  image->height = atoi(number);
+  free(number);
+
+  //Reading max color value
+  do{
+    fread(buffer, sizeof(char), 1, image->file);
+  }while(strcmp(buffer, "\n"));
+
+  free(buffer);
+  return image;
 }
 
 ppmImage convertImage(ppmImage inputImage, char* outputName, char conversionType){
@@ -140,7 +143,7 @@ ppmImage convertImage(ppmImage inputImage, char* outputName, char conversionType
   outputImage->name = outputName;
   outputImage->type = conversionType;
   outputImage->width = inputImage->width;
-  outputImage->height = outputImage->height;
+  outputImage->height = inputImage->height;
 
   //opening output file
   outputImage->file = fopen(outputName, "w");
@@ -148,6 +151,56 @@ ppmImage convertImage(ppmImage inputImage, char* outputName, char conversionType
     free(outputImage);
     return NULL;
   }
+
+  //Write header
+  fprintf(outputImage->file, "P%d\n#Wrtitten by PPMRW program made by Bruno TESSIER\n%d %d\n255\n", outputImage->type, outputImage->width, outputImage->height);
+
+
+  unsigned char* buffer = NULL;
+  if(inputImage->type == 3){
+    buffer = (unsigned char*)malloc(4 * sizeof(char));
+  }
+  else{
+    buffer = (unsigned char*)malloc(2 * sizeof(char));
+  }
+  int read = 1;
+  do{
+    //Read data
+    if(inputImage->type == 3){
+      buffer = fgets(buffer, 5, inputImage->file);
+    }
+    else{
+      read = fread(buffer, sizeof(char), 1, inputImage->file);
+    }
+
+    //Write data
+    if(buffer != NULL){
+      if(conversionType == 3){
+        //P3 to P3
+        if(inputImage->type == 3){
+          fprintf(outputImage->file, "%s",buffer);
+        }
+        //P6 to P3
+        else{
+          fprintf(outputImage->file, "%d\n", buffer[0]);
+        }
+      }
+      else{
+        //P3 to P6
+        if(inputImage->type == 3){
+          unsigned char *data = (char*)malloc(sizeof(char));
+          data[0] = (unsigned char) atoi(buffer);
+          fwrite(data, sizeof(char), 1, outputImage->file);
+          free(data);
+        }
+        //P6 to P6
+        else{
+          fwrite(buffer, sizeof(char), 1, outputImage->file);
+        }
+      }
+    }
+  }while(buffer != NULL && read != 0);
+  free(buffer);
 
   return outputImage;
 }
